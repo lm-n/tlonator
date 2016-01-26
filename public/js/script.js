c0a88b9b88ac1a7d344574cd4a7a7207b9304393
luis_morales_navarro <lmn297@nyu.edu>
2016-01-26 15:26:22 +0400

    optimized nlp

--- public/js/script.js
+++ public/js/script.js
@@ -7,28 +7,37 @@ var randomnumber;
 var thetlontext = "Their language and those things derived from their language—religion, literature, metaphysics—presuppose idealism. For the people of Tlön,the world is not an amalgam of objects in space; it is a heterogeneous series of independent acts the world is successive, temporal, but not spatial. There are no nouns in the conjectural Ursprache of Tlön,from which its present-day languages and dialects derive: there are impersonal verbs, modified by mono-syllabic suffixes (or prefixes) functioning as adverbs.";
 var theDT;
 var finaltext = "Click on the buttons to see examples of Tlönized English and learn more about the world of Tlön";
+var currentsentence;
+var ritatestPOS;
+var noundefinition;
+var theRitaTextArray = [];
+var dtplusPOSArray = [];
+var defs = [];
+var newTextArrays = [];
+var termAndDef = [];
+var nouns = [];
 
-function replacef(apiTermResponses){
+function replacef(termAndDef){
 	finaltext = thetlontext;
-	for (var i = 0; i < apiTermResponses.length; i++){
-		var curToBeRepl = apiTermResponses[i][0];
-		var curBecome = apiTermResponses[i][1];
-		finaltext = finaltext.replace(curToBeRepl, curBecome);
+	for (var i = 0; i < termAndDef.length; i++){
+		var noun = termAndDef[i][0];
+		var def = termAndDef[i][1];
+		finaltext = finaltext.replace(noun, def);
 	}
-	//console.log('textreplaced');
 	finaltext = finaltext.replace(/\s\s/g,' ');
-	//console.log(finaltext);
 	finaltextIsReady = true;
+
+	console.log(finaltext);
 }
 
-function getAPIDic(term, orderVal){
+function textprocess(term){
+	//console.log(term);
 	$.ajax({
-		//url: "http://cors.io/?u=http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + term + "?key=21960e29-e95e-456c-af22-7ff4c8b2fedb",
 		url: '/dict/' + term,
 		type: 'GET',
 		dataType: 'json',
 		error: function(data){
-				alert("Oh No! Try a refresh?");
+			alert("Oh No! Try a refresh?");
 			},
 		success: function(data){
 			//console.log(data);
@@ -40,57 +49,40 @@ function getAPIDic(term, orderVal){
 			else{
 				theDT = "";
 			}
-
-			var theNounArrayinDT = nlp.pos(theDT).nouns();
-			var theTextArrayinDT = [];
-			theNounArrayinDT.forEach(function(item){
-			theTextArrayinDT.push(item.text);
-			});
-
-			for (var g = 0; g < theTextArrayinDT.length; g++){
-				var curToBeRepl = theTextArrayinDT[g];
-				theDT = theDT.replace(curToBeRepl, '');
-			}
-
+			dtplusPOSArray = [];
+			tokenDT = [];
+			dtPOS = [];
+			//console.log(theDT);
+			
 			for (var h = 0; h < replaceterms.length; h++){
 				var curToBeRepla = replaceterms[h];
 				theDT = theDT.replace(curToBeRepla, '');
 			}
 
-			var tempArray = [term, theDT];
-			apiTermResponses.push(tempArray);
-			if (apiTermResponses.length == numRequests){
-				//console.log('allhere');
-				replacef(apiTermResponses);
-				//console.log(apiTermResponses);
-				//console.log('readytoreplaceterms');
+			var tokenDT = RiTa.tokenize(theDT);//separate the definition into an array of words
+			var dtPOS = RiTa.getPosTags(theDT);//get the parts of speech of each word
+
+			for (var w = 0; w < tokenDT.length; w++){
+				dtplusPOSArray.push([tokenDT[w], dtPOS[w]]); //populate the array with arrays made of each word and its part of speech
 			}
 
-			else{
-				//console.log("Not yet...");
+			//console.log(dtplusPOSArray);
+			defs = [];
+			for (var v = 0; v < dtplusPOSArray.length; v++){
+				if (dtplusPOSArray [v][1] == "nn" || dtplusPOSArray [v][1] == "nns" || dtplusPOSArray [v][1] == "nnp" || dtplusPOSArray [v][1] == "nnps" || dtplusPOSArray [v][1] == "prp" || dtplusPOSArray [v][1] == "prp$" ) {
+					dtplusPOSArray [v][0] = '';
+				}
+				defs.push(dtplusPOSArray [v][0]);
+			}
+			noundefinition = RiTa.untokenize(defs);
+			//console.log(noundefinition);
+			termAndDef.push([term, noundefinition]);
+
+			if (termAndDef.length == numRequests){
+				console.log(termAndDef);
+				replacef(termAndDef);
 			}
 		}
-	});
-}
-
-function nalapo(thetlontext){
-	var theNounArray = nlp.pos(thetlontext).nouns();
-	var theTextArray = [];
-		theNounArray.forEach(function(item){
-		theTextArray.push(item.text);
-	});
-	//console.log(theTextArray);
-	loadtext = true;
-	numRequests = theTextArray.length;
-	//console.log('allhere');
-	theTextArray.forEach(function(item,i){
-		var currentitem = item;
-		for (var f = 0; f < replaceterms.length; f++){
-			var curToBeRepl = replaceterms[f];
-			currentitem = currentitem.replace(curToBeRepl, '');
-		}
-		//console.log('nalapodone');
-		getAPIDic(currentitem,i);
 	});
 }
 
@@ -106,7 +98,20 @@ function getAPItexts(key){
 		success: function (resp) {
 			thetlontext = resp[randomnumber].doc.text;
 			thetlontextcat = resp[randomnumber].doc.namespace;
-			nalapo(thetlontext);
+			ritatestPOS = RiTa.getPosTags(thetlontext); //analize each sentence and indicate the part of speech of each word
+			tlonwords = RiTa.tokenize(thetlontext); //separate the sentence into words
+			for (var y = 0; y < tlonwords.length; y++){
+				theRitaTextArray.push([tlonwords[y], ritatestPOS[y]]); //populate the array with arrays made of each word and its part of speech
+			}
+			//console.log(theRitaTextArray);
+			for (var x = 0; x < theRitaTextArray.length; x++){
+				if (theRitaTextArray[x][1] == "nn" || theRitaTextArray[x][1] == "nns" || theRitaTextArray[x][1] == "nnp" || theRitaTextArray[x][1] == "nnps" || theRitaTextArray[x][1] == "prp" || theRitaTextArray[x][1] == "prp$") {
+					currentitem = theRitaTextArray[x][0];
+					textprocess(currentitem,x);
+					nouns.push(currentitem);
+				}
+			}
+			numRequests = nouns.length;
 		},
 	});
 }
